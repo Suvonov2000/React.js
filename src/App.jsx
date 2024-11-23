a885bb2373d84040835aab659a4cf5b53a75267d
Suvonov2000 <suvonov200@gmail.com>
2024-11-23 13:56:28 +0900

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Component } from "react";
+import Button from "./Button";
+import {plus} from "./Button"
 
 var user = {
-name:"umidjon",
-surname:"suvonov",
+name:"Umidjon",
+surname:"Suvonov",
+country:"Uzbekistan",
 };
 
 class App extends Component {
   render () {
-    return <div>2 + 2 = {2+2}
+    return <div>2 + 2 = {plus()}
     <h1 className="red_color">{user.name}</h1>
     <h1>{user.surname}</h1>
+    <h1>{user.country}</h1>
+    <Button />
     <div className="round"></div>
     </div>
   }
---
+++ src/Button.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+function plus () {
+    return 2 + 2;
+}
+
+
+class Button extends Component {
+    render () {
+        return <button>Click me</button>
+    }
+}
+
+export default Button;
+export {plus};
