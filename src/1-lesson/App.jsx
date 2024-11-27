b217b18cb63c99cea0b1a814d04aee85174fca15
Suvonov2000 <suvonov200@gmail.com>
2024-11-27 19:57:09 +0900

    react ex

--- src/1-lesson/App.jsx
+++ src/1-lesson/App.jsx
@@ -1,5 +1,16 @@
-function App () {
-    return <div>Hello from decloration</div>
+// function App () {
+//     return <div>Hello from decloration</div>
+// };
+
+
+// const App = function () {
+//     return <div>Hello from expretion </div>
+// };
+
+
+const App = () => {
+    return <div>Hello from Arrow function</div>
 }
+
 
 export default App;
