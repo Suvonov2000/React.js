d46c6bdd960d97248810cbd2b8b3bb8c88bcf929
Suvonov2000 <suvonov200@gmail.com>
2024-11-28 15:45:50 +0900

    react lessons

--- src/1-lesson/App.jsx
+++ src/1-lesson/App.jsx
@@ -3,14 +3,14 @@
 // };
 
 
-// const App = function () {
-//     return <div>Hello from expretion </div>
-// };
+const App = function () {
+    return <div>Hello from expretion </div>
+};
 
 
-const App = () => {
-    return <div>Hello from Arrow function</div>
-}
+// const App = () => {
+//     return <div>Hello from Arrow function</div>
+// }
 
 
-export default App;
+// export default App;
